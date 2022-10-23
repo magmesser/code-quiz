7198e594fa10509de20d86b4f042e994e249399a
magmesser <magmesser@gmail.com>
2022-10-23 18:21:58 -0400

    Added readme and completed code debug. Ready for submission.

--- README.md
+++ README.md
@@ -1,11 +1,61 @@
-# code-quiz
-Challenge 4
+# Code Quiz Project
+
+## User Story 
+>AS A coding boot camp student <br>
+I WANT to take a timed quiz on JavaScript fundamentals that stores high scores<br>
+SO THAT I can gauge my progress compared to my peers
+
+## Description
+
+This project tested my knowledge of JavaScript and APIs.  I ensured that the code was debugged to the best of my ability and ran correctly.  This assignment was very challenging due to implementing JS concepts that we have not yet covered in class. When I got stuck I used Google to research password generators and read through StackOverflow, and watched many YouTube videos, as well as researching specific elements in W3Schools and MDN. 
+
+I refererenced the following resources to assist in this project:
+* Referenced codestudio tutorial on how to write a javascript quiz: https://www.codingninjas.com/codestudio/library/how-to-create-a-javascript-quiz-code 
+* Referenced Build A Quizz App with JavaScript YouTube video from Web Dev Simplified: https://www.youtube.com/watch?v=riDzcEQbX6k 
+* Referenced Simple Countdown Timer with JavaScript YouTube video from Florin Pop: https://www.youtube.com/watch?v=x7WJEmxNlEs
+* Referenced Save High Scores in Local Storage YouTube video from James Q Quick: https://www.youtube.com/watch?v=DFhmNLKwwGw&list=PLDlWc9AfQBfZIkdVaOQXi1tizJeNJipEx&index=9
 
 
-Referenced codestudio tutorial on how to write a javascript quiz https://www.codingninjas.com/codestudio/library/how-to-create-a-javascript-quiz-code 
+---
 
-Referenced Build A Quizz App with JavaScript YouTube video from Web Dev Simplified. https://www.youtube.com/watch?v=riDzcEQbX6k 
+## Links
 
-Referenced Simple Countdown Timer with JavaScript YouTube video from Florin Pop. https://www.youtube.com/watch?v=x7WJEmxNlEs
+- Click the link below to access the deployed site on GitHub Pages.<br>
+https://magmesser.github.io/code-quiz/
 
-stackoverflow
+
+- Click the link below to access to application repository on GitHub. <br>
+https://github.com/magmesser/code-quiz
+
+
+---
+
+## Screenshot
+
+The following images are screenshots of the web application's appearance: 
+
+
+<br>
+Coding Quiz landing page
+
+![Screenshot 1 of the landing page](./Assets/readme_screenshots/screenshot1.png)
+
+<br>
+Correct answer choice reaction 
+
+![Screenshot 2 of a correct choice reaction](./Assets/readme_screenshots/screenshot2correct.png)
+
+<br>
+Wrong answer choice reaction
+
+![Screenshot 3 of a wrong choice reaction](./Assets/readme_screenshots/screenshot3wrong.png)
+
+<br>
+Game Over - completed quiz or timed out 
+
+![Screenshot 4 of the game over page](./Assets/readme_screenshots/screenshot4.png)
+
+<br>
+High Score scoreboard
+
+![Screenshot 5 of the scoreboard page](./Assets/readme_screenshots/screenshot5scoreboard.png)
--- script.js
+++ script.js
@@ -1,170 +1,270 @@
-// constant variables
-const restartBtn = document.getElementById('restart');
-const prevBtn = document.getElementById('prev');
-const nextBtn = document.getElementById('submit');
-const submitBtn = document.getElementById('submit');
-const trueBtn = document.getElementById('True');
-const falseBtn = document.getElementById('False');
-const userScore = document.getElementById('user-score');
-const questionText = document.getElementById('question-text');
+// What does the app need to do?
+// Start the game
+// Set next question 
+// Select Answer
+// Calculate Score
+// Interactive Timer
 
-// question counter
+// variables
 
-// score counter
+const introText = document.getElementById('intro-text');
+const startButton = document.getElementById('start-btn');
+const quizTimerElement = document.getElementById('timer');
+const nextButton = document.getElementById('next-btn');
+const questionContainerElement = document.getElementById('question-container');
+const questionElement = document.getElementById('question');
+const answerButtonsElement = document.getElementById('answer-buttons');
+const submitButton = document.getElementById('submit-btn');
+const finalScore = document.getElementById('final-score');
+const scoreboardElement = document.getElementById('scoreboard');
+const scoreboardList = document.getElementById('scoreboardList');
+const initials = document.querySelector('#initials');
+const mostRecentScore = localStorage.getItem('mostRecentScore');
+const scoreCounter = document.getElementById('score-counter');
+const MAX_HIGH_SCORES = 5;
+const highScoresList = JSON.parse(localStorage.getItem('highScoresList')) || [];
 
-// array of questions 
+var correctButton = document.getElementsByClassName('correct');
+var wrongButton = document.getElementsByClassName('wrong');
 
-let currentQuestion = 0;
-var score = 0;
+let score = 0;
+let shuffledQuestions, currentQuestionIndex, quizTimer;
+let selectedButton;
 
-let questions = [
-    // Question 1
+// events 
+
+startButton.addEventListener('click', startGame);
+startButton.addEventListener('click', startTimer);
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
+submitButton.addEventListener('click', scoreboard);
+
+
+// Function to start the game
+
+function startGame() {
+    console.log("Started")
+    startButton.classList.add('hide'),
+        introText.classList.add('hide'),
+        shuffledQuestions = questionsArray.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove('hide')
+    score = 0
+    quizTimerElement.classList.remove('hide')
+    scoreboardElement.classList.add('hide')
+    setNextQuestion()
+
+}
+
+function setNextQuestion() {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+    });
+}
+
+function resetState() {
+    clearStatusClass(document.body)
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
+}
+
+function selectAnswer(e) {
+    var selectedButton = e.target
+    var correct = selectedButton.dataset.correct
+
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+
+    // increase score if statement
+    if (correct) {
+        score = score + 5; //increases scoreCounter by 5 for correct answer
+    } else {
+        time = time - 15; //time penalty by 15 seconds for wrong answer
+    }
+    localStorage.setItem('mostRecentScore', score);
+   
+    // end of questions 
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove('hide')
+    } else {
+        showScore();
+        time = 0;
+    }
+}
+
+
+// timer function and variables
+
+const startingMinutes = 2;
+let time = startingMinutes * 60;
+
+function startTimer() {
+    var countdown = setInterval(updateTimer, 1000)
+
+    function updateTimer() {
+        const minutes = Math.floor(time / 60);
+        let seconds = time % 60;
+        var correct = answerButtonsElement.dataset.correct;
+
+        seconds = seconds < 10 ? '0' + seconds : seconds;
+
+        quizTimerElement.innerHTML = `Timer: ${minutes}:${seconds}`;
+        time--;
+
+        if (time < 0) {
+            clearInterval(countdown);
+            alert("GAME OVER")
+            showScore()
+        } 
+    
+        console.log(time);
+    }
+}
+
+
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
+
+// shows final score landing page 
+function showScore() {
+    finalScore.classList.remove('hide')
+    questionContainerElement.classList.add('hide')
+    quizTimerElement.classList.add('hide')
+    nextButton.classList.add('hide')
+    clearStatusClass(document.body)
+}
+
+// scoreboard 
+
+scoreCounter.innerHTML = mostRecentScore;
+
+// cannot figure out why local storage mostRecentScore is correct, but display scoreCounter = mostRecentScore, is showing the previous player's score. 
+// cannot figure out why the current mostRecentScore is not displaying on the scoreboard until the following round 
+
+initials.addEventListener('keyup', () => {
+    console.log(initials.value);
+    submitButton.disabled = !initials.value;
+})
+
+saveHighScore = (e) => {
+    console.log("clicked submit");
+    e.preventDefault();
+
+    const score = {
+        score: mostRecentScore,
+        name: initials.value
+    };
+    highScoresList.push(score);
+    highScoresList.sort((a,b) => {
+        return b.score - a.score;
+    });
+    highScoresList.splice(5);
+    localStorage.setItem('highScoresList', JSON.stringify(highScoresList));
+
+    highScoresList.push(mostRecentScore);
+    console.log(highScoresList);
+};
+
+// next page - Scoreboard 
+scoreboardList.innerHTML = 
+highScoresList.map(score => {
+   return `<li class="name-score">${score.name} - ${score.score}</li>`;
+}).join("");
+console.log(scoreboardList);
+
+
+function scoreboard() {
+    finalScore.classList.add('hide')
+    scoreboardElement.classList.remove('hide')
+    // startButton.innerHTML = "Restart"
+    // startButton.classList.remove('hide')
+    // startButton.addEventListener('click', startGame)
+}
+
+// Questions 
+
+const questionsArray = [
     {
-        question: "Example Question 1?",
+        question: "Inside which HTML element do you place your JavaScript code?",
         answers: [
-            {option:"Answer A", answer:true},
-            {option:"Answer B", answer:false}
+            { text: "<script>", correct: true },
+            { text: "<meta>", correct: false },
+            { text: "<link>", correct: false },
+            { text: "<head>", correct: false }
         ]
     },
-    // Question 2
     {
-        question: "Example Question 2?",
+        question: "What selector would you use to select a class in css?",
         answers: [
-            {option:"Answer A", answer:true},
-            {option:"Answer B", answer:false}
+            { text: "#", correct: false },
+            { text: "*", correct: false },
+            { text: ":", correct: false },
+            { text: ".", correct: true }
         ]
     },
-     // Question 3
-     {
-        question: "Example Question 3?",
+    {
+        question: "What operator would you use to make the expression true? 8 __ '8' ",
         answers: [
-            {option:"Answer A", answer:true},
-            {option:"Answer B", answer:false}
+            { text: "=", correct: false },
+            { text: "==", correct: true },
+            { text: "===", correct: false },
+            { text: "=>", correct: false }
+        ]
+    },
+    {
+        question: "A variable that is defined within a function is known as _____ scope?",
+        answers: [
+            { text: "Outside", correct: false },
+            { text: "Inside", correct: false },
+            { text: "Local", correct: true },
+            { text: "Global", correct: false }
+        ]
+    },
+    {
+        question: "Which index is the letter 'D' on from the following array? ['A', 'B', 'C', 'D'] ",
+        answers: [
+            { text: "2", correct: false },
+            { text: "1", correct: false },
+            { text: "3", correct: true },
+            { text: "0", correct: false }
         ]
     }
 ]
 
-// click events
-restartBtn.addEventListener('click', restart);
-prevBtn.addEventListener('click', prev);
-nextBtn.addEventListener('click', next);
-submitBtn.addEventListener('click', submit);
-
-// main quiz function
-function beginQuiz() {
-    currentQuestion = 0;
-    questionText.innerHTML = questions[currentQuestion].question;
-    trueBtn.innerHTML = questions[currentQuestion].answers[0].option;
-    trueBtn.onclick = () => {
-        let ano=0;
-        if(questions[currentQuestion].answers[ano].answer) {
-            if(score < 3) {
-                score++;
-            }
-        }
-        userScore.innerHTML = score;
-        if(currentQuestion < 2) {
-            next();
-        }
-    }
-    prevBtn.classList.add('hide');
-}
-
-beginQuiz();
-
-// restart the quiz function
-function restart() {
-    currentQuestion = 0;
-    prevBtn.classList.remove('hide');
-    nextBtn.classList.remove('hide');
-    submitBtn.classList.remove('hide');
-    trueBtn.classList.remove('hide');
-    falseBtn.classList.remove('hide');
-    score = 0;
-    userScore.innerHTML = score;
-    beginQuiz();
-}
-
-// next function to move to next question
-function next() {
-    currentQuestion++;
-    if(currentQuestion >= 2){
-        nextBtn.classList.add('hide');
-        prevBtn.classList.remove('hide');
-    }
-    questionText.innerHTML = questions[currentQuestion].question;
-    trueBtn.innerHTML = questions[currentQuestion].answers[0].option;
-    trueBtn.onclick = () => {
-        let ano = 0;
-        if (questions[currentQuestion].answers[ano].answer){
-            if(score < 3){
-                score++;
-            }
-        }
-        userScore.innerHTML = score;
-        if(currentQuestion < 2 ){
-            next();
-        }
-    }
-    falseBtn.innerHTML = questions[currentQuestion].answers[1].option;
-    falseBtn.onclick = () => {
-        let ano = 1;
-        if (questions[currentQuestion].answers[ano].answer){
-            if(score < 3){
-                score++;
-            }
-        }
-        userScore.innerHTML = score;
-        if(currentQuestion < 2 ){
-            next();
-        }
-    }
-    prevBtn.classList.remove('hide');
-}
-
-// previous function to move to previous question 
-function prev() {
-    currentQuestion--;
-    if(currentQuestion <= 0){
-        prevBtn.classList.add('hide');
-        nextBtn.classList.remove('hide');
-    }
-    questionText.innerHTML = questions[currentQuestion].question;
-    trueBtn.innerHTML = questions[currentQuestion].answers[0].option;
-    trueBtn.onclick = () => {
-        let ano = 0;
-        if (questions[currentQuestion].answers[ano].answer){
-            if(score < 3){
-                score++;
-            }
-        }
-        userScore.innerHTML = score;
-        if(currentQuestion < 2 ){
-            next();
-        }
-    }
-    falseBtn.innerHTML = questions[currentQuestion].answers[1].option;
-    falseBtn.onclick = () => {
-        let ano = 1;
-        if (questions[currentQuestion].answers[ano].answer){
-            if(score < 3){
-                score++;
-            }
-        }
-        userScore.innerHTML = score;
-        if(currentQuestion < 2 ){
-            next();
-        }
-    }
-    nextBtn.classList.remove('hide');
-}
-
-// submit function to complete quiz
-function submit(){
-    prevBtn.classList.add('hide');
-    nextBtn.classList.add('hide');
-    submitBtn.classList.add('hide');
-    trueBtn.classList.add('hide');
-    falseBtn.classList.add('hide');
-    questionText.innerHTML = "Congratulations on completing the quiz!"
-}
+// questions draft area:
+// "Inside which HTML element do you place your JavaScript code?" "<script>"** "<meta>" "<link>" "<head>"
+// "What selector would you use to select a class in css?" "#"  "*" ":" "."**
+// "What operator would you use to make the expression true? 8 __ '8' " "=" "=="** "===" "=>"
+// A variable that is defined within a function is known as _____ scope? "Outside" "Inside" "Local"** "Global"
+// Which index is the letter 'D' on from the following array: ['A', 'B', 'C', 'D']?  "2", "1", "3",** "0"
